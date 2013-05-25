d573e8e7fc663e2957f63516ffe2fef05cf8f937
Rachel Fish <itsrachelfish@gmail.com>
2013-05-25 01:19:24 -0600

    THE BOT ACTUALLY CONNECTS OMG

--- bot.js
+++ bot.js
@@ -35,14 +35,26 @@ request.get('https://www.icanhazchat.com/signin', function (error, response, bod
                     {
                         var room = $(body);
 
-                        room.find('input[name="ctl00$ContentPlaceHolder1$txtUserName"]').val(config.username + "text");
+                        room.find('input[name="ctl00$ContentPlaceHolder1$txtUserName"]').val(config.username);
                         room.find('input[name="ctl00$ContentPlaceHolder1$txtPassword"]').val(config.roomPass);
                         room.find('input[name="ctl00$ContentPlaceHolder1$backImgUrl"]').val('');
-                        room.find('input[name="ctl00$ContentPlaceHolder1$btnLogin"]').val("I am ready, let's go!");
-                        room.find('input[name="WHARBARGLpass"]').val('a6c22d12c0105ae2d3a18d2ad0c22492dd1b4ae');
+                        room.find('input[name="ctl00$ContentPlaceHolder1$btnLogin"]').val("I am 18+ and agree to the rules");
+
+                        room.find('input[name="ctl00$ContentPlaceHolder1$btnGoAway"]').remove();
+                        room.find('input[name="ctl00$btnAmazonSearch"]').remove();
+
                         
                         
-                        var postdata = {};
+                        room.find('input[name="WHARBARGLpass"]').val('0e14b468e465b0f47e6f141786e34a7fe4216d77');
+                        
+                        
+                        var postdata =
+                        {
+                            __LASTFOCUS: '',
+                            __EVENTTARGET: '',
+                            __EVENTARGUMENT: ''
+                        };
+
 
                         // Loop through all inputs to build a response (ichc has a lot of hidden values)
                         room.find('input').each(function()
@@ -50,31 +62,36 @@ request.get('https://www.icanhazchat.com/signin', function (error, response, bod
                             $(this).removeAttr('disabled');
                             postdata[$(this).attr('name')] = $(this).val();
                         });
-                                                
-                        // Log into the room
-                        request.post('https://www.icanhazchat.com/?room=' + config.room, {form: postdata}, function (error, response, body)
+
+                        console.log(postdata);
+
+                        setTimeout(function()
                         {
-                            // We can't check for statusCode == 200 here because the room password page issues a 302 redirect
-                            if(!error)
+                            // Log into the room
+                            request.post('https://www.icanhazchat.com/?room=' + config.room, {form: postdata}, function (error, response, body)
                             {
-                                var wharb = $(body).find('#WHARBARGL1').val();
-                                
-                                request.cookie("thumper=" + wharb + "; path=/");
-                                request.cookie("linestyle=3; path=/");
-                                request.cookie("http://www.icanhazchat.com/ec_png=" + wharb + "; path=/");
-                                request.cookie("http://www.icanhazchat.com/ec_etag=" + wharb + "; path=/");
-                                request.cookie("http://www.icanhazchat.com/ec_cache=" + wharb + ";  path=/");
-                                request.cookie("show_images=1; path=/");
-                                
-                                request.get('https://www.icanhazchat.com/' + config.room, function (error, response, body)
+                                // We can't check for statusCode == 200 here because the room password page issues a 302 redirect
+                                if(!error)
                                 {
-                                    if (!error && response.statusCode == 200)
+                                    var wharb = $(body).find('#WHARBARGL1').val();
+
+                                    request.cookie("thumper=" + wharb + "; path=/");
+                                    request.cookie("linestyle=3; path=/");
+                                    request.cookie("http://www.icanhazchat.com/ec_png=" + wharb + "; path=/");
+                                    request.cookie("http://www.icanhazchat.com/ec_etag=" + wharb + "; path=/");
+                                    request.cookie("http://www.icanhazchat.com/ec_cache=" + wharb + ";  path=/");
+                                    request.cookie("show_images=1; path=/");
+                                    
+                                    request.get('https://www.icanhazchat.com/' + config.room, function (error, response, body)
                                     {
-                                        console.log(body);
-                                    }
-                                });
-                            }
-                        });
+                                        if (!error && response.statusCode == 200)
+                                        {
+                                            console.log(body);
+                                        }
+                                    });
+                                }
+                            });
+                        }, 5000);
                     }
                     else
                     {
---
+++ post.js
@@ -0,0 +1,37 @@
+var http = require('http');
+var querystring = require('querystring');
+
+function postRequest(request, response, callback) {
+    var queryData = "";
+    if(typeof callback !== 'function') return null;
+
+    if(request.method == 'POST') {
+        request.on('data', function(data) {
+            queryData += data;
+            if(queryData.length > 1e6) {
+                queryData = "";
+                response.writeHead(413, {'Content-Type': 'text/plain'}).end();
+                request.connection.destroy();
+            }
+        });
+
+        request.on('end', function() {
+            response.post = querystring.parse(queryData);
+            callback();
+        });
+
+    } else {
+        response.writeHead(405, {'Content-Type': 'text/plain'});
+        response.end();
+    }
+}
+
+http.createServer(function(request, response) {
+    postRequest(request, response, function() {
+        console.log(response.post);
+
+        response.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+        response.end();
+    });
+
+}).listen(1024);
